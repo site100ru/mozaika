89200964b74b8f135c9c7dfff07bb70710f3ef8a
Roman <150604774+vasilyev-r@users.noreply.github.com>
2026-05-15 21:45:24 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -181,6 +181,27 @@ class bootstrap_5_wp_nav_menu_walker extends Walker_Nav_menu {
 
 
 
+
+/* Register a new menu */
+add_action( 'after_setup_theme', function() {
+	register_nav_menus( [
+		'main-menu' => 'Main menu',
+		'mobail-header-collapse' => 'Mobail header collapse',
+		'sliding-header-collapse' => 'Sliding header collapse',
+		'contacts-desktop-menu' => 'Contacts desktop menu',
+		'footer-right' => 'footer-right',
+		'footer-left' => 'footer-left',
+		//'menu-main-menu-2' => 'Menu main menu 2',
+		//'menu-main-menu-3' => 'Menu main menu 3',
+		//'contacts-menu-2' => 'Contacts menu 2',
+		//'navbarSupportedContent2' => 'navbarSupportedContent2'
+
+	] );
+} );
+
+
+
+
 /*** ON WooCommerce support ***/
 /* WooCommerce support */
 add_action( 'after_setup_theme', 'furniture_catalog_add_woocommerce_support' );
@@ -188,6 +209,51 @@ function furniture_catalog_add_woocommerce_support() {
 	add_theme_support( 'woocommerce' );
 }
 /*** END ON WooCommerce support ***/
+
+
+
+
+/* Изменяем размер миниатюр WooCommerce */
+add_filter('woocommerce_get_image_size_thumbnail','add_thumbnail_size',1,10);
+function add_thumbnail_size($size){
+	$size['width'] = 600;
+	//$size['height'] = 450;
+	$size['crop']   = 1; //0 - не обрезаем, 1 - обрезка
+	return $size;
+}
+
+/* Отключаем ненужные опции вывода настраницу */
+/* Отключаем название товара
+remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_rating', 10 );
+
+remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_price', 10);
+remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 20);
+add_action('woocommerce_single_product_summary', 'woocommerce_template_single_price', 20);
+add_action('woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 10);
+
+remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_upsell_display', 15 ); */
+remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs', 10 );
+remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40 );
+
+// Цена
+remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_price', 10);
+//add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 20 );
+
+// Кнопка
+remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30);
+/* End отключаем ненужные опции вывода настраницу */
+
+
+
+
+/* Изменям значек валюты */
+add_filter('woocommerce_currency_symbol', 'change_existing_currency_symbol', 10, 2);
+function change_existing_currency_symbol( $currency_symbol, $currency ) {
+		switch( $currency ) {
+			case 'RUB': $currency_symbol = '₽'; break;
+		}
+		return $currency_symbol;
+}
 
 
 
@@ -386,6 +452,35 @@ function register_post_types() {
 	] );
 }
 /*** END REGISTER POST TYPE ***/
+
+
+
+
+/* Wijet область в сайдбаре */
+if ( function_exists( 'register_sidebar' ) ) {
+	register_sidebar(
+		array(
+			'name'          => 'Виджет в сайдбаре', //название виджета в админ-панели
+			'id'            => 'wsidebar-1', //идентификатор виджета
+			'description'   => 'Показывается в категории Кухни', //описание виджета в админ-панели
+			'before_widget' => '<aside id="%1$s" class="widget %2$s">', //открывающий тег виджета с динамичным идентификатором
+			'after_widget'  => '<div class="clear"></div></aside>', //закрывающий тег виджета с очищающим блоком
+			'before_title'  => '<span class="widget-title">', //открывающий тег заголовка виджета
+			'after_title'   => '</span>',//закрывающий тег заголовка виджета
+		)
+	);
+
+	/* Виджет для шкафов
+	register_sidebar(array(
+		'name'          => 'Сайдбар Шкафы',
+		'id'            => 'wsidebar-2',
+		'description'   => 'Показывается в категории Шкафы',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '<div class="clear"></div></aside>',
+		'before_title'  => '<span class="widget-title">',
+		'after_title'   => '</span>',
+	));*/
+}
 
 
 
@@ -1080,3 +1175,51 @@ function mytheme_get_emails_extra() {
     return is_array($emails) ? $emails : array();
 }
 /*** ДЕЛАЕМ ВСЕ ЧТО СВЯЗАНО С КОНТАКТАМИ ***/
+
+
+
+
+// Подключаем функцию активации мета блока (my_extra_fields)
+add_action('add_meta_boxes', 'my_extra_fields', 1);
+
+function my_extra_fields() {
+	add_meta_box( 'extra_fields', 'Галерея наших работ', 'extra_fields_box_func', 'portfolio', 'side', 'high' );
+}
+
+/* Код блока галереи */
+function extra_fields_box_func( $post ){
+	for ($i=1; $i<=9; $i++) { ?>
+		<label>URL&#160;изображения <?php echo $i; ?>:</label>
+		<input type="text" name="extra[img-<?php echo $i; ?>]" value="<?php echo get_post_meta($post->ID, '_img-'.$i, 1); ?>" style="width: 100%;">
+		<div style="clear: both;"></div>
+	<?php } ?>
+		<input type="hidden" name="extra_fields_nonce" value="<?php echo wp_create_nonce(__FILE__); ?>" />
+	<?php
+}
+
+// включаем обновление полей при сохранении
+add_action( 'save_post', 'my_extra_fields_update', 0 );
+
+## Сохраняем данные, при сохранении поста
+function my_extra_fields_update( $post_id ){
+	// базовая проверка
+	if (
+			empty( $_POST['extra'] )
+		|| ! wp_verify_nonce( $_POST['extra_fields_nonce'], __FILE__ )
+		|| wp_is_post_autosave( $post_id )
+		|| wp_is_post_revision( $post_id )
+	)
+		return false;
+
+	// Все ОК! Теперь, нужно сохранить/удалить данные
+	//$_POST['extra'] = array_map( 'sanitize_text_field', $_POST['extra'] ); // чистим все данные от пробелов по краям
+	foreach( $_POST['extra'] as $key => $value ){
+		if( empty($value) ){
+			delete_post_meta( $post_id, '_'.$key ); // удаляем поле если значение пустое
+			continue;
+		}
+		update_post_meta( $post_id, '_'.$key, $value ); // add_post_meta() работает автоматически
+	}
+	return $post_id;
+}
+/* END подключаем функцию активации мета блока (my_extra_fields) */
