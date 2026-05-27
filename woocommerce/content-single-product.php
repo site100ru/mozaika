70b0460acfec91cb4f6374dfad256fcb92820e72
SlavaWebDeveloper <sidorov-vv3@mail.ru>
2026-05-27 20:22:34 +0500

    подправил высоту от шапки до контента на мобильном устройстве

--- woocommerce/content-single-product.php
+++ woocommerce/content-single-product.php
@@ -33,15 +33,15 @@ if ( post_password_required() ) {
 ?>
 
 <section class="single-product-section bg-white">
-	<div id="product-<?php the_ID(); ?>" <?php wc_product_class( 'pt-md-3', $product ); ?>>
-		<div class="container pt-md-3 pb-5 single-product">
+	<div id="product-<?php the_ID(); ?>" <?php wc_product_class( 'pt-3', $product ); ?>>
+		<div class="container pt-3 pb-5 single-product">
 			<div class="row">
 				<div class="col breadcrumbs_box">
 					<div class="row">
-                        <div class="breadcrumbs" >
-                            <?php woocommerce_breadcrumb(); ?>
-                        </div>
-                    </div>
+						<div class="breadcrumbs">
+							<?php woocommerce_breadcrumb(); ?>
+						</div>
+					</div>
 				</div>
 			</div>
 			<div class="row justify-content-start">
@@ -56,7 +56,7 @@ if ( post_password_required() ) {
 					do_action( 'woocommerce_before_single_product_summary' );
 					?>
 				</div>
-                <div class="col-md-5" style="width: 100%; max-width: 100%; flex: 1; padding-right: 0;">
+				<div class="col-md-5" style="width: 100%; max-width: 100%; flex: 1; padding-right: 0;">
 					<div class="summary entry-summary">
 						<div class="row pt-5 pt-lg-0 w-100">
 							<div class="col text-dark text-start" style="padding-right: 0;">
@@ -87,7 +87,7 @@ if ( post_password_required() ) {
 
 <!-- Gallery wrapper -->
 <div id="galleryWrapper" style="background: rgba(0,0,0,0.85); display: none; position: fixed; top: 0; bottom: 0; left: 0; right: 0; z-index: 9999;">
-	
+
 	<div id="gallery-<?php the_ID(); ?>" class="carousel slide" data-bs-ride="false" data-bs-interval="false" style="display: none; position: fixed; top: 0; height: 100%; width: 100%;">
 		<div class="carousel-inner h-100">
 			<?php
@@ -111,15 +111,15 @@ if ( post_password_required() ) {
 			$count = $count + 1;
 		}
 		if ( $count > 1 ) { ?>
-			<button class="carousel-control-prev" type="button" data-bs-target="#gallery-<?php the_ID(); ?>" data-bs-slide="prev">
-				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
-				<span class="visually-hidden">Previous</span>
-			</button>
-			<button class="carousel-control-next" type="button" data-bs-target="#gallery-<?php the_ID(); ?>" data-bs-slide="next">
-				<span class="carousel-control-next-icon" aria-hidden="true"></span>
-				<span class="visually-hidden">Next</span>
-			</button>
-			<?php }
+		<button class="carousel-control-prev" type="button" data-bs-target="#gallery-<?php the_ID(); ?>" data-bs-slide="prev">
+			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			<span class="visually-hidden">Previous</span>
+		</button>
+		<button class="carousel-control-next" type="button" data-bs-target="#gallery-<?php the_ID(); ?>" data-bs-slide="next">
+			<span class="carousel-control-next-icon" aria-hidden="true"></span>
+			<span class="visually-hidden">Next</span>
+		</button>
+		<?php }
 		?>
 	</div>
 
@@ -147,8 +147,8 @@ if ( post_password_required() ) {
 		document.getElementById("gallery-<?php the_ID(); ?>").style.display = "none";
 	}
 </script>
-				          				
-													
+
+
 <!--h2 class="product_title text-corporate-color-2 mb-3">Кухня #099</h2>
 <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/ico/section-title-dec.svg" style="margin-bottom: 30px;">
 <div class="woocommerce-product-details__short-description">
@@ -168,7 +168,7 @@ if ( post_password_required() ) {
 	</div>
 </div-->
 
-							
+
 <?php
 /**
  * Hook: woocommerce_after_single_product_summary.
